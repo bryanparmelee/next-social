974b40a2652323ee8836021e506f161b04483951
bryanparmelee <bryan.parmelee@gmail.com>
2023-06-24 21:49:07 -0400

    Add footer, navbar components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Navbar from "@/components/NavBar";
+import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata = {
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,50 @@
 import React from "react";
+import Image from "next/image";
+import { footerLinks } from "@/constants";
+import Link from "next/link";
+
+type ColumnProps = {
+  title: string;
+  links: Array<string>;
+};
+
+const FooterColumn = ({ title, links }: ColumnProps) => (
+  <div className="footer-column">
+    <h4 className="font-semibold">{title}</h4>
+    <ul className="flex flex-col gap-2 font-normal">
+      {links.map((link) => (
+        <Link href="/" key={link}>
+          {link}
+        </Link>
+      ))}
+    </ul>
+  </div>
+);
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flexStart footer">
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image
+            src="/logo-purple.svg"
+            width={115}
+            height={38}
+            alt="Next Social"
+          />
+        </div>
+        <div className="flex flex-wrap gap-12">
+          <FooterColumn
+            title={footerLinks[0].title}
+            links={footerLinks[0].links}
+          />
+        </div>
+      </div>
+      <div className="flexBetween footer-copyright">
+        <p>&copy; 2023</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
